65e3a4361419354bb071c45bc33ad911b1c46668
nitishgupta978 <nitishgupta9785@gmail.com>
2022-05-31 13:05:42 +0530

    Print Hello

---
+++ src/main/java/com/bridgelabz/greetingcontrollerapp/GreetingcontrollerappApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingcontrollerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingcontrollerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingcontrollerappApplication.class, args);
+	}
+
+}
