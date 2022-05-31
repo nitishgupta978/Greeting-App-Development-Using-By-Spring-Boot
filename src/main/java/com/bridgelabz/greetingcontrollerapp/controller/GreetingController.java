39b362cea3ed73749f746cd6dcd03d38495811ef
nitishgupta978 <nitishgupta9785@gmail.com>
2022-05-31 16:12:21 +0530

    added: Name based on user attributes provide or just Hello World:

--- src/main/java/com/bridgelabz/greetingcontrollerapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingcontrollerapp/controller/GreetingController.java
@@ -1,9 +1,9 @@
 package com.bridgelabz.greetingcontrollerapp.controller;
 
 import com.bridgelabz.greetingcontrollerapp.model.Greeting;
-//import com.bridgelabz.greetingcontrollerapp.model.User;
-//import com.bridgelabz.greetingcontrollerapp.service.IGreetingService;
-//import org.springframework.beans.factory.annotation.Autowired;
+import com.bridgelabz.greetingcontrollerapp.model.User;
+import com.bridgelabz.greetingcontrollerapp.service.IGreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,20 +15,20 @@ import java.util.concurrent.atomic.AtomicLong;
 @RequestMapping("/greeting")
 public class GreetingController {
 
-    private  static final  String template = "Hello,%s,%s !";
-    private final AtomicLong counter = new AtomicLong();
+//    private  static final  String template = "Hello,%s,%s !";
+//    private final AtomicLong counter = new AtomicLong();
 //    both private transfer/use in service so comment
 
-//    @Autowired
-//    private IGreetingService greetingService;
+    @Autowired
+    private IGreetingService greetingService;
     @GetMapping(value = {"","/"})
     public Greeting greeting(@RequestParam(value = "fName", defaultValue = "Word!!") String fname,
                              @RequestParam (value = "lName", defaultValue = "")String lname){
-       return new Greeting(counter.incrementAndGet(),String.format(template,fname,lname));
-//        User user = new User();
-//        user.setFirstName(fname);
-//        user.setLastName(lname);
-//        return greetingService.addGreeting(user);
+//        return new Greeting(counter.incrementAndGet(),String.format(template,fname,lname));
+        User user = new User();
+        user.setFirstName(fname);
+        user.setLastName(lname);
+        return greetingService.addGreeting(user);
 
 
     }
--- src/main/java/com/bridgelabz/greetingcontrollerapp/service/GreetingServiceImpl.java
+++ src/main/java/com/bridgelabz/greetingcontrollerapp/service/GreetingServiceImpl.java
@@ -1,25 +1,25 @@
-//package com.bridgelabz.greetingcontrollerapp.service;
-//
-//import com.bridgelabz.greetingcontrollerapp.model.Greeting;
-//import com.bridgelabz.greetingcontrollerapp.model.User;
-//import com.bridgelabz.greetingcontrollerapp.repository.IGreetingRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.concurrent.atomic.AtomicLong;
-//
-//@Service
-//public class GreetingServiceImpl implements IGreetingService{
-//
-//
-//    @Autowired
-//    private IGreetingRepository greetingRepository;
-//    private  static final  String template = "Hello,%s,%s !";
-//    private final AtomicLong counter = new AtomicLong();
-//    @Override
-//    public Greeting addGreeting(User user) {
-//        String message = String.format(template,(user.toString().isEmpty()) ?"Hello World!":user.toString());
-//        return greetingRepository.save(new Greeting(counter.incrementAndGet(),message));
-//    }
-//}
-//
+package com.bridgelabz.greetingcontrollerapp.service;
+
+import com.bridgelabz.greetingcontrollerapp.model.Greeting;
+import com.bridgelabz.greetingcontrollerapp.model.User;
+import com.bridgelabz.greetingcontrollerapp.repository.IGreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class GreetingServiceImpl implements IGreetingService{
+
+
+    @Autowired
+    private IGreetingRepository greetingRepository;
+    private  static final  String template = "Hello,%s,%s !";
+    private final AtomicLong counter = new AtomicLong();
+    @Override
+    public Greeting addGreeting(User user) {
+        String message = String.format(template,(user.toString().isEmpty()) ?"Hello World!":user.toString());
+        return greetingRepository.save(new Greeting(counter.incrementAndGet(),message));
+    }
+}
+
